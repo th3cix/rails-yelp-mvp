4599c5a5d48ba6e057acb047993df672af658361
Giacomo Paolozzi <giacomo@gingerweb.it>
2018-04-11 15:34:13 +0200

    partial completed

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,29 @@
+# app/controllers/reviews_controller.rb
+class ReviewsController < ApplicationController
+	def new
+    # we need @restaurant in our `simple_form_for`
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+  	@review = Review.new(review_params)
+    # we need `restaurant_id` to asssociate review with corresponding restaurant
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    if @review.save
+    	redirect_to restaurant_path(@restaurant)
+    else
+    	render :new
+    end
+  end
+  def show
+  end
+
+
+  private
+
+  def review_params
+  	params.require(:review).permit(:content, :rating)
+  end
+end
